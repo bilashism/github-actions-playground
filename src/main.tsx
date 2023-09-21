c65a4a98795da43f8ab9ffc643586f63c3b90092
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2023-09-21 15:16:22 +0600

    feat: kghfguf

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 
-import { store } from './redux/store';
 import { App } from './App';
 
 import './index.css';
@@ -12,8 +10,6 @@ const rootEl = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootEl);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
